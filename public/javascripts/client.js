1e1b6e9b51cd4cbe446396ca9f8bba4019a0cca6
Roberto Ortega <berto.ort@gmail.com>
2015-07-29 12:02:46 -0600

    beautiful data

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -529,18 +529,150 @@ var analyze = document.querySelector('.analyze');
 if (analyze) {
   analyze.addEventListener('click', function (e) {
     var games = document.querySelector('.games');
+    var row = document.querySelector('.row');
     var gif = document.querySelector('.gif');
+    var gamesNumber = games.value;
     gif.style.display = 'inline-block';
     var xhr = new XMLHttpRequest();
-    xhr.open('get', '/simulate?games=' + games.value)
+    xhr.open('get', '/simulate?games=' + gamesNumber)
     xhr.addEventListener('load', function () {
-      var response = xhr.response;
-      gif.style.display = 'none';
+      var response = JSON.parse(xhr.response);
       var reset = document.querySelector('.reset');
-      reset.style.display = 'inline-block';
       var form = document.querySelector('.form');
+      var graphs = document.querySelector('.graphs');
+      gif.style.display = 'none';
       form.style.display = 'none';
-      console.log(response);
+      reset.style.display = 'inline-block';
+      graphs.style.display = 'inline-block';
+      row.style.display = 'block';
+      var data = [ {letter: "rounds" + " " + response.rounds, frequency: response.rounds},
+                   {letter: "wars" + " " + response.wars, frequency: response.wars},
+                   {letter: "doubleWars" + " " + response.doubleWars, frequency: response.doubleWars}
+                   ];
+      var data2 = [
+                   {letter: '2-2 Aces' + " " + response.aces[2].total, frequency: response.aces[2].total},
+                   {letter: '1-3 Aces' + " " + response.aces[1].total, frequency: response.aces[1].total},
+                   {letter: '0-4 Aces' + " " + response.aces[0].total, frequency: response.aces[0].total},
+                  ];
+      //Bar Graph 1
+
+      var margin = {top: 20, right: 20, bottom: 30, left: 40},
+          width = 500 - margin.left - margin.right,
+          height = 400 - margin.top - margin.bottom;
+
+      var x = d3.scale.ordinal()
+          .rangeRoundBands([0, width], .1);
+
+      var y = d3.scale.linear()
+          .range([height, 0]);
+
+      var xAxis = d3.svg.axis()
+          .scale(x)
+          .orient("bottom");
+
+      var yAxis = d3.svg.axis()
+          .scale(y)
+          .orient("left")
+          .ticks(10);
+
+      var svg = d3.select(".graph1").append("svg")
+          .attr("width", width + margin.left + margin.right)
+          .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+      x.domain(data.map(function(d) { return d.letter; }));
+      y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
+
+      svg.append("g")
+          .attr("class", "x axis")
+          .attr("transform", "translate(0," + height + ")")
+          .call(xAxis);
+
+      svg.append("g")
+          .attr("class", "y axis")
+          .call(yAxis)
+        .append("text")
+          .attr("transform", "rotate(-90)")
+          .attr("y", 6)
+          .attr("dy", ".71em")
+          .style("text-anchor", "end")
+          .text("Total");
+
+      svg.selectAll(".bar")
+          .data(data)
+        .enter().append("rect")
+          .attr("class", "bar")
+          .attr("x", function(d) { return x(d.letter); })
+          .attr("width", x.rangeBand())
+          .attr("y", function(d) { return y(d.frequency); })
+          .attr("height", function(d) { return height - y(d.frequency); });
+
+      // bar graph 2
+
+      var margin = {top: 20, right: 20, bottom: 30, left: 40},
+          width = 500 - margin.left - margin.right,
+          height = 300 - margin.top - margin.bottom;
+
+      var x = d3.scale.ordinal()
+          .rangeRoundBands([0, width], .1);
+
+      var y = d3.scale.linear()
+          .range([height, 0]);
+
+      var xAxis = d3.svg.axis()
+          .scale(x)
+          .orient("bottom");
+
+      var yAxis = d3.svg.axis()
+          .scale(y)
+          .orient("left")
+          .ticks(10);
+
+      var svg = d3.select(".graph2").append("svg")
+          .attr("width", width + margin.left + margin.right)
+          .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+      x.domain(data2.map(function(d) { return d.letter; }));
+      y.domain([0, d3.max(data2, function(d) { return d.frequency; })]);
+
+      svg.append("g")
+          .attr("class", "x axis")
+          .attr("transform", "translate(0," + height + ")")
+          .call(xAxis);
+
+      svg.append("g")
+          .attr("class", "y axis")
+          .call(yAxis)
+        .append("text")
+          .attr("transform", "rotate(-90)")
+          .attr("y", 6)
+          .attr("dy", ".71em")
+          .style("text-anchor", "end")
+          .text("Total");
+
+      svg.selectAll(".bar")
+          .data(data2)
+        .enter().append("rect")
+          .attr("class", "bar")
+          .attr("x", function(d) { return x(d.letter); })
+          .attr("width", x.rangeBand())
+          .attr("y", function(d) { return y(d.frequency); })
+          .attr("height", function(d) { return height - y(d.frequency); });
+
+      function type(d) {
+        d.frequency = +d.frequency;
+        return d;
+      }
+
+      loadLiquidFillGauge("zeroGauge", response.aces[0].won);
+      loadLiquidFillGauge("oneGauge", response.aces[1].won);
+      loadLiquidFillGauge("twoGauge", response.aces[2].won);
+      loadLiquidFillGauge("threeGauge", response.aces[3].won);
+      loadLiquidFillGauge("fourGauge", response.aces[4].won);
+      console.log(response, gamesNumber, response.aces[0].start);
     })
     xhr.send();
     e.preventDefault();
